3873558feda19957ef348b8ba4eeeb030a5dffd5
jakubjakubowski8 <jakubjakubowski8@gmail.com>
2021-01-13 16:08:49 +0100

    Release v1.0.0

--- creat_docker_image.sh
+++ creat_docker_image.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-TAG="0.0.1-SNAPSHOT"
+TAG="1.0.0"
 DOCKER_USER=user
 DOCKER_PASSWORD=changeme
 ./mvnw spring-boot:build-image
 docker image tag xmlreader:${TAG} jakubjakubowski/xmlreader:${TAG}
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-docker image push jakubjakubowski/xmlreader:0.0.1-SNAPSHOT
+docker image push jakubjakubowski/xmlreader:${TAG}
 docker logout
